213e722c614a9100acaee1d11a3af3fd0b39c773
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-05-18 20:38:30 +0000

    deno port: part 2

--- transform.ts
+++ transform.ts
@@ -22,44 +22,64 @@ const results: ResultsWrapper = resultsWrapperSchema.parse(
 	JSON.parse(await Deno.readTextFile(filename)),
 )
 
+const runCommandScriptColor = async (command: string): Promise<string> => {
+	const tmpfile_path = await Deno.makeTempFile({ suffix: "error" })
+	const tmpfile = await Deno.open(
+		tmpfile_path,
+		{
+			read: true,
+			write: true,
+			create: true,
+		},
+	)
+	const process = new Deno.Command("script", {
+		args: [
+			"-efq",
+			"-c",
+			command,
+		],
+		env: {
+			"TERM": "xterm-256color",
+		},
+		stdin: "null",
+		stdout: "piped",
+		stderr: "piped",
+	}).spawn()
+	const joined = mergeReadableStreams(
+		process.stdout,
+		process.stderr,
+	)
+	joined.pipeTo(tmpfile.writable)
+	await process.status
+	try {
+		await Deno.remove("./typescript")
+	} catch (e) {
+		if (e instanceof Deno.errors.NotFound) {
+			// ignore
+		} else {
+			throw e
+		}
+	}
+	const output = await Deno.readTextFile(tmpfile_path)
+	await Deno.remove(tmpfile_path)
+	return output
+}
+
 results.results = await Promise.all(results.results.map(async (status) => {
 	switch (status.type) {
 		case "EVAL": {
 			if (status.success) {
 				return status
 			} else {
-				const process = new Deno.Command("script", {
-					args: [
-						"-efq",
-						"-c",
-						`nix eval --show-trace ".#checks.${status.attr}"`,
-					],
-					env: {
-						"TERM": "xterm-256color",
-					},
-					stdin: "null",
-					stdout: "piped",
-					stderr: "piped",
-				}).spawn()
-				const joined = mergeReadableStreams(
-					process.stdout,
-					process.stderr,
+				status.error = await runCommandScriptColor(
+					`nix eval --show-trace ".#checks.${status.attr}"`,
 				)
-				const tmpfile_path = await Deno.makeTempFile({ suffix: "error" })
-				const tmpfile = await Deno.open(
-					tmpfile_path,
-					{
-						read: true,
-						write: true,
-						create: true,
-					},
-				)
-				joined.pipeTo(tmpfile.writable)
-				await process.status
-				//await Deno.remove("./typescript")
-				status.error = await Deno.readTextFile(tmpfile_path)
 				return status
 			}
+		}
+		case "BUILD": {
+			status.error = await runCommandScriptColor(`nix log ".#checks.${status.attr}"`)
+			return status
 		}
 		default: {
 			return status
@@ -67,4 +87,7 @@ results.results = await Promise.all(results.results.map(async (status) => {
 	}
 }))
 
-await Deno.writeTextFile("./result_parsed.json", JSON.stringify(results, null, "\t"))
+await Deno.writeTextFile(
+	"./result_parsed.json",
+	JSON.stringify(results, null, "\t"),
+)
