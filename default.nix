cc81910386f29147f4f0069bb0e3eb8884d7e845
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2025-05-20 16:51:24 +0000

    artifact

---
+++ default.nix
@@ -0,0 +1,69 @@
+{
+  makeNixAction =
+    {
+      useLix ? false,
+      shouldInstall ? true,
+      preBuild ? [ ],
+      postUpload ? [ ],
+      runs-on ? "ubuntu-latest",
+      arch ? "x86_64-linux",
+      flake,
+    }:
+    {
+      jobs = {
+        fast-build = {
+          steps = [
+            {
+              uses = "actions/checkout@v4";
+            }
+            {
+              uses = "DeterminateSystems/nix-installer-action@main";
+              "with" = {
+                source-url = "https://install.lix.systems/lix/lix-installer-\${{ fromJSON('{\"X64\":\"x86_64\",\"X86\":\"i686\",\"ARM64\":\"aarch64\",\"ARM\":\"armv7l\"}')[runner.arch] }}-\${{ fromJSON('{\"Linux\":\"linux\",\"macOS\":\"darwin\",\"Windows\":\"windows\"}')[runner.os] }}";
+                logger = "pretty";
+                diagnostic-endpoint = "";
+              };
+            }
+            {
+              name = "nix-fast-build";
+              run = "nix run nixpkgs#lixPackageSets.latest.nix-fast-build -- --no-nom --flake \".#checks.$(nix eval --raw --impure --expr builtins.currentSystem)\" --result-file result.json || true";
+            }
+            {
+              name = "nix log";
+              run = "nix log .#checks.x86_64-linux.pre-commit";
+            }
+            {
+              name = "transform";
+              run = "nix shell nixpkgs#unixtools.script nixpkgs#nushell --command nu ./transform.nu result.json";
+            }
+            {
+              name = "upload artifact";
+              uses = "actions/upload-artifact@v4";
+              "with" = {
+                name = "results.json";
+                path = "./result_parsed.json";
+              };
+            }
+          ];
+        };
+        download-artifact = {
+          needs = [ "fast-build" ];
+          steps = [
+            {
+              uses = "actions/checkout@v4";
+            }
+            {
+              uses = "actions/download-artifact@v4";
+              "with" = {
+                path = "artifacts";
+              };
+            }
+            {
+              name = "ls";
+              run = "ls artifacts/";
+            }
+          ];
+        };
+      };
+    };
+}
--- flake.nix
+++ flake.nix
@@ -43,45 +43,7 @@
           };
         };
         workflows = {
-          ".github/workflows/main.yaml" = {
-            jobs = {
-              fast-build = {
-                steps = [
-                  {
-                    uses = "actions/checkout@v4";
-                  }
-                  {
-                    uses = "DeterminateSystems/nix-installer-action@main";
-                    "with" = {
-                      source-url = "https://install.lix.systems/lix/lix-installer-\${{ fromJSON('{\"X64\":\"x86_64\",\"X86\":\"i686\",\"ARM64\":\"aarch64\",\"ARM\":\"armv7l\"}')[runner.arch] }}-\${{ fromJSON('{\"Linux\":\"linux\",\"macOS\":\"darwin\",\"Windows\":\"windows\"}')[runner.os] }}";
-                      logger = "pretty";
-                      diagnostic-endpoint = "";
-                    };
-                  }
-                  {
-                    name = "nix-fast-build";
-                    run = "nix run nixpkgs#lixPackageSets.latest.nix-fast-build -- --no-nom --flake \".#checks.$(nix eval --raw --impure --expr builtins.currentSystem)\" --result-file result.json || true";
-                  }
-                  {
-                    name = "nix log";
-                    run = "nix log .#checks.x86_64-linux.pre-commit";
-                  }
-                  {
-                    name = "transform";
-                    run = "nix shell nixpkgs#unixtools.script nixpkgs#nushell --command nu ./transform.nu result.json";
-                  }
-                  {
-                    name = "upload artifact";
-                    uses = "actions/upload-artifact@v4";
-                    "with" = {
-                      name = "results.json";
-                      path = "./result_parsed.json";
-                    };
-                  }
-                ];
-              };
-            };
-          };
+          ".github/workflows/main.yaml" = (import ./default.nix).makeNixAction { flake = self; };
         };
       };
 
